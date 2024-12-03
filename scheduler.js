0bb58075feb77574f74f00afb83c72255e7ffe44
waqqasiq <waqqas.ess@gmail.com>
2024-12-04 01:41:38 +0600

    feed url set in env

--- scheduler.js
+++ scheduler.js
@@ -2,11 +2,12 @@ const schedule = require('node-schedule');
 const { fetchArticles } = require('./services/rssService'); // Adjust the path to your service
 const { saveArticles } = require('./services/articleService'); // Adjust the path to your service
 
+
 // Schedule the task to run at the start of every hour
 schedule.scheduleJob('*/2 * * * *', async () => {
     console.log('Fetching articles...');
     try {
-        const articles = await fetchArticles('https://www.wired.com/feed/rss');
+        const articles = await fetchArticles(process.env.FEED_CHANNEL_URL);
         await saveArticles(articles); // Logic to save articles in MySQL
         console.log('Articles saved successfully!');
     } catch (error) {
